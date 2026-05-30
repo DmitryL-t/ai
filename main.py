12e29f3d5e6e1b8741e5f0b4ebb9e903aaf80f95
DmitryL-t <yu.e.shestov@gmail.com>
2026-05-30 21:18:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,38 @@ from random import random
 
 class Ai:
 	def __init__(self, genes):
+		# len(self.genes) = 16
 		self.genes = genes
 
 	def answer(self, input_lst):
 		genes = self.genes
-		input_lst += [1, -1]
+		input_lst.append(1)
+		# len(input_lst) == 3
 		A = [0, 0, 0, 0]
 		for i in range(4):
-			for j in range(4):
-				A[i] += genes[4 * i + j] * input_lst[j]
+			for j in range(3):
+				A[i] += genes[3 * i + j] * input_lst[j]
 		A = [sin(i) for i in A]
 		B = 0
 		for i in range(4):
-			B += genes[16 + i] * A[i]
+			B += genes[12 + i] * A[i]
 		
 		return B
 
-	def how_good(self, d):
+	def score(self, d):
 		inp, right_output = d
 		output = self.answer(inp)
 		return (output - right_output) ** 2
 
-	def how_good_all(self, data):
+	def score_all(self, data):
 		return sum([
-			self.how_good(d) for d in data
+			self.score(d) for d in data
 		])
 	def c(self):
 		x = random()
 		genes1 = self.genes[:]
 		for i in range(len(genes1)):
-			if random() < 0.25:
+			if random() < 0.2:
 				genes1[i] += 0.1 * (random() - 1 / 2)
 		return Ai(genes1)
 
@@ -42,14 +44,19 @@ data.append(([0, 1], 1))
 data.append(([1, 0], 1))
 data.append(([1, 1], 0))
 
-ai = Ai([random() - 1 / 2 for _ in range(20)])
-for i in range(100):
-	if i % 10 == 0:
-		print(i, ai.how_good_all(data))
+ai = Ai([random() - 1 / 2 for _ in range(16)])
+score = ai.score_all(data)
+print(score)
+best_score = score
+for i in range(1000):
 	ai_list = [ai.c() for _ in range(10)] + [ai]
-	ai = sorted(ai_list, key=lambda ai:ai.how_good_all(data))[0]
+	ai = sorted(ai_list, key=lambda ai:ai.score_all(data))[0]
+	score = ai.score_all(data)
+	if score < best_score:
+		print(i, ai.score_all(data))
+		best_score = score 
 
-print(ai.how_good_all(data))
+print(ai.score_all(data))
 print('>', ai.answer([0, 0]))
 print('>', ai.answer([0, 1]))
 print('>', ai.answer([1, 0]))
